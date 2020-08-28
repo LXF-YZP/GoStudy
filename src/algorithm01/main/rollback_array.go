05b927317eb5921b252576024f236950418da4b0
yuezhiping <yuezhiping@bonc.com.cn>
2020-08-28 14:02:41 +0800

    提交几个算法题

---
+++ src/algorithm01/main/rollback_array.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	int2 := []int{1, 2, 3}
+	fmt.Println(int2)
+	int3 := rollbackArray(int2)
+	fmt.Println(int3)
+}
+func rollbackArray(nums []int) []int {
+	var i, j = 0, len(nums) - 1
+	for i < j {
+		nums[i], nums[j] = nums[j], nums[i]
+		i++
+		j--
+	}
+	return nums
+}
